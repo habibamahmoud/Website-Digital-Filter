408c2b5759366cd93d8d41e66657228c6014538d
habibamahmoud <habiba.ahmed98@eng-st.cu.edu.eg>
2021-06-21 23:17:50 +0200

    connect

--- main.py
+++ main.py
@@ -2,78 +2,99 @@ import numpy as np
 import cmath as math
 import matplotlib.pyplot as plt
 from scipy import signal
-from flask import Flask, render_template , request
+from flask import Flask, render_template, request
 angel = np.arange(0, 181, 1)
 app = Flask(__name__, template_folder='html', static_folder='static')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+fig = plt.figure()
 
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-def get_mag(zeros : list ,poles : list):
-    transfer_function=get_transfer_function(zeros, poles)
-    mag=np.absolute(transfer_function)
-    fig=plt.figure()
-    plt.plot(angel*np.pi/180,mag)
+
+def get_mag(zeros: list, poles: list):
+    transfer_function = get_transfer_function(zeros, poles)
+    mag = np.absolute(transfer_function)
+    fig = plt.figure()
+    plt.plot(angel*np.pi/180, mag)
     plt.ylabel('frequency')
-    plt.savefig('mag.png')
-    # plt.show()
+    plt.savefig('static/img/mag.png')
 
-def get_phase(zeros : list ,poles : list):
-    transfer_function=get_transfer_function(zeros,poles)
-    phase=np.angle(transfer_function)
-    fig=plt.figure()
-    plt.plot(angel*np.pi/180,phase)
+
+def get_phase(zeros: list, poles: list):
+    transfer_function = get_transfer_function(zeros, poles)
+    phase = np.angle(transfer_function)
+    fig = plt.figure()
+    plt.plot(angel*np.pi/180, phase)
     plt.ylabel('phase')
-    plt.savefig('phase.png')
-    # plt.show()
+    plt.savefig('static/img/phase.png')
 
-def all_passfilter(zeros: list,poles : list ,a: float):
-    filtered_transfer_function= get_transfer_function(zeros, poles)
-    filtered_transfer_function*=1-np.exp(1j*angel*np.pi/180)*np.complex(a,0)
-    filtered_transfer_function/=np.exp(1j*angel*np.pi/180)-np.complex(a,0)
-    fig=plt.figure()
-    plt.plot(angel*np.pi/180,np.angle(filtered_transfer_function))
-    plt.ylabel('filtered phase')
-    plt.savefig('filtered.png')
-    return filtered_transfer_function
-    # plt.show()
 
+def all_passfilter(zeros: list, poles: list, z: list, p: list):
+    filtered_transfer_function = get_transfer_function(zeros, poles)
+    try:
+        for i in z:
+            mag = 1/(((i[0])**2+(i[1])**2) ** 0.5)
+            phase = -1 * math.tan(i[0]/i[1])
+            poles.append([mag*math.cos(phase), mag*math.sin(phase)])
+            filtered_transfer_function /= np.exp(1j *
+                                                 angel*np.pi/180) - (mag * np.exp(1j*phase))
+    except:
+        try:
+            for i in p:
+                mag = 1/(((i[0])**2+(i[1])**2) ** 0.5)
+                phase = -1 * math.tan(i[0]/i[1])
+                zeros.append([mag*math.cos(phase), mag*math.sin(phase)])
+                filtered_transfer_function *= np.exp(
+                    1j*angel*np.pi/180) - (mag * np.exp(1j*phase))
+        except:
+            pass
+    filtered_phase = np.angle(filtered_transfer_function)
+    plt.plot(angel*np.pi/180, filtered_phase)
+    plt.ylabel('filtered_phase')
+    plt.savefig('static/img/after_filter_phase.png')
+
+    return zeros, poles
 
 
 def get_transfer_function(zeros, poles):
-    transfer_function=1
+    transfer_function = 1
     for i in zeros:
-        transfer_function*=np.exp(-1j*angel*np.pi/180)-np.complex(i[0],i[1])
+        transfer_function *= np.exp(-1j*angel*np.pi/180)-np.complex(i[1], i[0])
     for i in poles:
-        transfer_function/=np.exp(-1j*angel*np.pi/180)-np.complex(i[0],i[1])
+        transfer_function /= np.exp(-1j*angel*np.pi/180)-np.complex(i[1], i[0])
     return transfer_function
 
-def begin(zeros,poles,a):
-    get_mag(zeros,poles)
-    get_phase(zeros,poles)
-    all_passfilter(zeros,poles,a)
 
-@app.route('/',methods=["GET"])
+def begin(zeros: list, poles: list, z: list, p: list):
+    get_mag(zeros, poles)
+    get_phase(zeros, poles)
+    zeros, poles = all_passfilter(zeros, poles, z, p)
+    return zeros, poles
+
+
+@app.route('/', methods=["GET"])
 def index():
     return render_template("index.html")
-@app.route('/',methods=["POST"])
+
+
+@app.route('/', methods=["POST"])
 def running():
-    zeros=request.form["zeros"]
-    poles=request.form["poles"]
-    a=request.form["all pass filter constant"]
-    begin(zeros,poles,a)
-    return render_template("index.html",zeros=zeros,poles=poles,a=a)
+    zeros = request.get_json().get("zeros")
+    poles = request.get_json().get("poles")
+    z = request.get_json().get("zeros_out_of_range")
+    p = request.get_json().get("poles_out_of_range")
+    print(f"received request with parameters: zeros={zeros}, poles={poles}")
+    zeros, poles = begin(zeros, poles, z, p)
+    return render_template("index.html", zeros=zeros, poles=poles)
 
 
 if __name__ == '__main__':
     app.run()
-
-
-# def double_filter(zeros: list,poles : list ,a: float,filtered_tf: list):
-#     double_transfer_function=filtered_tf
-#     double_transfer_function*=1-np.exp(1j*angel*np.pi/180)*np.complex(a,0)
-#     double_transfer_function/=np.exp(1j*angel*np.pi/180)-np.complex(a,0)
-#     fig=plt.figure()
-#     plt.plot(angel*np.pi/180,np.angle(double_transfer_function))
-#     plt.ylabel('filtered phase')
-#     plt.savefig('double_filtered.png')
-#     return double_transfer_function
--- static/js/main.js
+++ static/js/main.js
@@ -7,267 +7,247 @@ zeroFlag = false;
 poleFlag = false;
 
 var conjucateFlag = false;
-var poles = []
-var tempPoles = []
-var Truepoles = []
-var zeros = []
-var tempZeros = []
-var Truezeros = []
-var conjucatedZeros = []
-var conjucatedPoles = []
-var Conj_Poles = []
-var Conj_Zeros = []
+var poles = [];
+var tempPoles = [];
+var Truepoles = [];
+var zeros = [];
+var z = [];
+var p = [];
+var tempZeros = [];
+var Truezeros = [];
+var conjucatedZeros = [];
+var conjucatedPoles = [];
+var Conj_Poles = [];
+var Conj_Zeros = [];
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 Draw();
 
 function Draw() {
-    ctx.clearRect(0, 0, c.width, c.height);
-    
-    var pad = (c.width - 2 * radius) / 2; // padding on each side
-    
-    // unit circle
+  ctx.clearRect(0, 0, c.width, c.height);
+
+  var pad = (c.width - 2 * radius) / 2; // padding on each side
+
+  // unit circle
+  ctx.beginPath();
+  ctx.strokeStyle = "red";
+  ctx.arc(radius + pad, radius + pad, radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  // y axis
+  ctx.beginPath();
+  //ctx.lineWidth="1";
+  ctx.strokeStyle = "lightgray";
+  ctx.moveTo(radius + pad, 0);
+  ctx.lineTo(radius + pad, c.height);
+  ctx.font = "italic 8px sans-serif";
+  ctx.fillText("Imaginary", radius + pad + 2, pad - 2);
+
+  // x axis
+  ctx.moveTo(0, radius + pad);
+  ctx.lineTo(c.width, radius + pad);
+  ctx.fillText("Real", radius + radius + pad + 2, radius + pad - 2);
+  ctx.stroke(); // Draw it
+  ctx.strokeStyle = "blue";
+  var idx;
+  Truepoles = [];
+  for (idx = 0; idx < poles.length; idx++) {
+    var x = poles[idx][0] - 10;
+    var y = poles[idx][1] - 10;
+
+    Truepoles.push([(x - 22 - radius) / radius, (radius - (y - 22)) / radius]);
+
     ctx.beginPath();
-    ctx.strokeStyle = "red";
-    ctx.arc(radius + pad, radius + pad, radius, 0, 2 * Math.PI);
+    ctx.moveTo(x - pSize, y - pSize);
+    ctx.lineTo(x + pSize, y + pSize);
+    ctx.moveTo(x - pSize, y + pSize);
+    ctx.lineTo(x + pSize, y - pSize);
     ctx.stroke();
-    // y axis
+  }
+  Truezeros = [];
+  for (idx = 0; idx < zeros.length; idx++) {
+    var x = zeros[idx][0] - 10;
+    var y = zeros[idx][1] - 10;
+
+    Truezeros.push([(x - 22 - radius) / radius, (radius - (y - 22)) / radius]);
+
     ctx.beginPath();
-    //ctx.lineWidth="1";
-    ctx.strokeStyle = "lightgray";
-    ctx.moveTo(radius + pad, 0);
-    ctx.lineTo(radius + pad, c.height);
-    ctx.font = "italic 8px sans-serif";
-    ctx.fillText("Imaginary", radius + pad + 2, pad - 2);
-    
-    // x axis
-    ctx.moveTo(0, radius + pad);
-    ctx.lineTo(c.width, radius + pad);
-    ctx.fillText("Real", radius + radius + pad + 2, radius + pad - 2);
-    ctx.stroke(); // Draw it
-    ctx.strokeStyle = "blue";
-    var idx;
-    Truepoles = []
-    for (idx = 0; idx < poles.length; idx++) {
-        var x = poles[idx][0] - 10;
-        var y = poles[idx][1] - 10;
-        
-        Truepoles.push([((x-22) - radius) / radius, (radius - (y-22)) / radius]);
-        
-        ctx.beginPath();
-        ctx.moveTo(x - pSize , y - pSize );
-        ctx.lineTo(x + pSize , y + pSize );
-        ctx.moveTo(x - pSize , y + pSize );
-        ctx.lineTo(x + pSize , y - pSize );
-        ctx.stroke();
-    };
-    Truezeros = []
-    for (idx = 0; idx < zeros.length; idx++) {
-        var x = zeros[idx][0] - 10;
-        var y = zeros[idx][1] - 10;
-        
-        Truezeros.push([((x-22) - radius) / radius, (radius - (y-22)) / radius]);
-        
-        ctx.beginPath();
-        ctx.arc(x , y , zSize, 0, 2 * Math.PI);
-        ctx.stroke();
-    };
-};
+    ctx.arc(x, y, zSize, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+}
 
 function AddPoles() {
-    var x = radius + (radius * 0.75) ;
-    var y = radius - (radius * 0.34) ;
-    poles.push([x + 32, y + 32]);
-    Draw();
-};
+  var x = radius + radius * 0.75;
+  var y = radius - radius * 0.34;
+  poles.push([x + 32, y + 32]);
+  Draw();
+}
 
 function AddZeros() {
-    var x = radius + (radius * 0) ;
-    var y = radius - (radius * 0) ;
-    zeros.push([x + 32, y + 32]);
-    Draw();
-};
-function updateMenu(){
-
+  var x = radius + radius * 0;
+  var y = radius - radius * 0;
+  zeros.push([x + 32, y + 32]);
+  Draw();
 }
+function updateMenu() {}
 function showCoords(event) {
-    var x = event.offsetX;
-    var y = event.offsetY;
-    if (zeroFlag){
-        zeros[currentIndx][0] = x
-        zeros[currentIndx][1] = y
-        tempZeros = zeros
-        Draw()
-        for(var idx=0; idx<conjucatedZeros.length; idx++){
-            if (conjucatedZeros[idx][0] === currentIndx){
-                var conjIdx = conjucatedZeros[idx][1];
-                Truezeros[conjIdx][0] = Truezeros[currentIndx][0]
-                Truezeros[conjIdx][1] = -Truezeros[currentIndx][1]
-                tempx = radius + (radius * Truezeros[conjIdx][0]);
-                tempy = radius - (radius * Truezeros[conjIdx][1]);
-                zeros[conjIdx] = [tempx + 32,tempy + 32];
-            }
-        }
+  var x = event.offsetX;
+  var y = event.offsetY;
+
+  if (zeroFlag) {
+    zeros[currentIndx][0] = x;
+    zeros[currentIndx][1] = y;
+    tempZeros = zeros;
+    Draw();
+    for (var idx = 0; idx < conjucatedZeros.length; idx++) {
+      if (conjucatedZeros[idx][0] === currentIndx) {
+        var conjIdx = conjucatedZeros[idx][1];
+        Truezeros[conjIdx][0] = Truezeros[currentIndx][0];
+        Truezeros[conjIdx][1] = -Truezeros[currentIndx][1];
+        tempx = radius + radius * Truezeros[conjIdx][0];
+        tempy = radius - radius * Truezeros[conjIdx][1];
+        zeros[conjIdx] = [tempx + 32, tempy + 32];
+      }
     }
-    if (poleFlag){
-        poles[currentIndx][0] = x
-        poles[currentIndx][1] = y
-        tempPoles = poles
-        Draw()
-        for(var idx=0; idx<conjucatedPoles.length; idx++){
-            if (conjucatedPoles[idx][0] === currentIndx){
-                var conjIdx = conjucatedPoles[idx][1];
-                Truepoles[conjIdx][0] = Truepoles[currentIndx][0]
-                Truepoles[conjIdx][1] = -Truepoles[currentIndx][1]
-                tempx = radius + (radius * Truepoles[conjIdx][0]);
-                tempy = radius - (radius * Truepoles[conjIdx][1]);
-                poles[conjIdx] = [tempx + 32,tempy + 32];
-            }
-        }
+  }
+  if (poleFlag) {
+    poles[currentIndx][0] = x;
+    poles[currentIndx][1] = y;
+    tempPoles = poles;
+    Draw();
+    for (var idx = 0; idx < conjucatedPoles.length; idx++) {
+      if (conjucatedPoles[idx][0] === currentIndx) {
+        var conjIdx = conjucatedPoles[idx][1];
+        Truepoles[conjIdx][0] = Truepoles[currentIndx][0];
+        Truepoles[conjIdx][1] = -Truepoles[currentIndx][1];
+        tempx = radius + radius * Truepoles[conjIdx][0];
+        tempy = radius - radius * Truepoles[conjIdx][1];
+        poles[conjIdx] = [tempx + 32, tempy + 32];
+      }
     }
-    Draw()
+  }
+  Draw();
 }
 
-function Conjugate(){
-    conjucateFlag = true
-    var x;
-    var y;
-    var tempx;
-    var tempy;
-    if (zeroFlag){
-        x = Truezeros[currentIndx][0]
-        y = Truezeros[currentIndx][1]
-        if (!Truezeros.includes([x, -y]) && !Conj_Zeros.includes(currentIndx)){
-            tempx = radius + (radius * x);
-            tempy = radius - (radius * -y);
-            zeros.push([tempx + 32,tempy + 32]);
-            conjucatedZeros.push([currentIndx, zeros.length-1])
-            Conj_Zeros.push(currentIndx)
-        }
+function Conjugate() {
+  conjucateFlag = true;
+  var x;
+  var y;
+  var tempx;
+  var tempy;
+  if (zeroFlag) {
+    x = Truezeros[currentIndx][0];
+    y = Truezeros[currentIndx][1];
+    if (!Truezeros.includes([x, -y]) && !Conj_Zeros.includes(currentIndx)) {
+      tempx = radius + radius * x;
+      tempy = radius - radius * -y;
+      zeros.push([tempx + 32, tempy + 32]);
+      conjucatedZeros.push([currentIndx, zeros.length - 1]);
+      Conj_Zeros.push(currentIndx);
     }
-    else if(poleFlag){
-        x = Truepoles[currentIndx][0]
-        y = Truepoles[currentIndx][1]
-        if (!Truepoles.includes([x, -y]) && !Conj_Poles.includes(currentIndx)){
-            tempx = radius + (radius * x);
-            tempy = radius - (radius * -y);
-            poles.push([tempx + 32,tempy + 32]);
-            conjucatedPoles.push([currentIndx, poles.length-1])
-            Conj_Poles.push(currentIndx)
-        }
+  } else if (poleFlag) {
+    x = Truepoles[currentIndx][0];
+    y = Truepoles[currentIndx][1];
+    if (!Truepoles.includes([x, -y]) && !Conj_Poles.includes(currentIndx)) {
+      tempx = radius + radius * x;
+      tempy = radius - radius * -y;
+      poles.push([tempx + 32, tempy + 32]);
+      conjucatedPoles.push([currentIndx, poles.length - 1]);
+      Conj_Poles.push(currentIndx);
     }
-    Draw()
+  }
+  Draw();
 }
 
-function clearSelected(){
-    if (zeroFlag){
-        zeros.splice(currentIndx,1);
-        for(var idx=0; idx<conjucatedZeros.length; idx++){
-            if (conjucatedZeros[idx][0] === currentIndx){
-                var conjIdx = conjucatedZeros[idx][1]-1;
-                zeros.splice(conjIdx,1);
-                conjucatedZeros.splice(conjIdx,1);
-            }
-        }
-        Draw()
+function clearSelected() {
+  if (zeroFlag) {
+    zeros.splice(currentIndx, 1);
+    for (var idx = 0; idx < conjucatedZeros.length; idx++) {
+      if (conjucatedZeros[idx][0] === currentIndx) {
+        var conjIdx = conjucatedZeros[idx][1] - 1;
+        zeros.splice(conjIdx, 1);
+        conjucatedZeros.splice(conjIdx, 1);
+      }
     }
-    else if(poleFlag){
-        poles.splice(currentIndx,1);
-        for(var idx=0; idx<conjucatedPoles.length; idx++){
-            if (conjucatedPoles[idx][0] === currentIndx){
-                var conjIdx = conjucatedPoles[idx][1]-1 ;
-                poles.splice(conjIdx,1);
-                conjucatedPoles.splice(conjIdx,1);
-            }
-        }
-        Draw()
+    Draw();
+  } else if (poleFlag) {
+    poles.splice(currentIndx, 1);
+    for (var idx = 0; idx < conjucatedPoles.length; idx++) {
+      if (conjucatedPoles[idx][0] === currentIndx) {
+        var conjIdx = conjucatedPoles[idx][1] - 1;
+        poles.splice(conjIdx, 1);
+        conjucatedPoles.splice(conjIdx, 1);
+      }
     }
+    Draw();
+  }
+  calculate();
 }
 function cleaar(index) {
-    if (index == 1){
-        zeros = [];
-        conjucatedZeros = [];
-        Conj_Zeros = [];
+  if (index == 1) {
+    zeros = [];
+    conjucatedZeros = [];
+    Conj_Zeros = [];
     Draw();
-    }
-    if (index == 2){
-        poles = [];
-        conjucatedPoles = [];
-        Conj_Poles = [];
-        Draw();
-    }
-    if (index == 3){
-        poles = [];
-        zeros = [];
-        conjucatedZeros = [];
-        conjucatedPoles = [];
-        Conj_Zeros = [];
-        Conj_Poles = [];
-        Draw();
-    }
+  }
+  if (index == 2) {
+    poles = [];
+    conjucatedPoles = [];
+    Conj_Poles = [];
+    Draw();
+  }
+  if (index == 3) {
+    poles = [];
+    zeros = [];
+    conjucatedZeros = [];
+    conjucatedPoles = [];
+    Conj_Zeros = [];
+    Conj_Poles = [];
+    Draw();
+  }
+  calculate();
 }
-    google.charts.load('current', { 'packages': ['corechart'] });
-    google.charts.setOnLoadCallback(drawChart);
-    google.charts.setOnLoadCallback(drawChart_1);
-    google.charts.setOnLoadCallback(drawChart_2);
 
-  function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-      ['Year', 'Sales', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
-    ]);
+function normalizePoint(p) {
+  return [(p[0] - 132) / 100, (-p[1] + 132) / 100];
+}
 
-    var options = {
-      title: 'Poles & Zeros',
-      curveType: 'function',
-      legend: { position: 'bottom' }
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
-
-    chart.draw(data, options);
+function calculate() {
+  if (zeros.length == 0 && poles.length == 0) clearPlots();
+  else {
+    axios
+      .post("/", {
+        zeros: zeros.map((el) => normalizePoint(el)),
+        poles: poles.map((el) => normalizePoint(el)),
+        z: z.map((el) => normalizePoint(el)),
+        p: p.map((el) => normalizePoint(el)),
+      })
+      .then(() => {
+        refreshPlots();
+      });
   }
-  function drawChart_1() {
-    var data = google.visualization.arrayToDataTable([
-      ['Year', 'Sales', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
-    ]);
+}
 
-    var options = {
-      title: 'Poles & Zeros',
-      curveType: 'function',
-      legend: { position: 'bottom' }
-    };
+function refreshPlots() {
+  [
+    ["plt1", "static/img/phase.png"],
+    ["plt2", "static/img/mag.png"],
+    ["plt3", "static/img/after_filter_phase.png"],
+  ].forEach((plt) => {
+    document
+      .getElementById(plt[0])
+      .setAttribute("src", plt[1] + "?t=" + new Date().getTime());
+  });
+}
 
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart_1'));
-
-    chart.draw(data, options);
-  }
-  function drawChart_2() {
-    var data = google.visualization.arrayToDataTable([
-      ['Year', 'Sales', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
-    ]);
-
-    var options = {
-      title: 'Poles & Zeros',
-      curveType: 'function',
-      legend: { position: 'bottom' }
-    };
-
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart_2'));
-
-    chart.draw(data, options);
-  }
-  
-
-  
+function clearPlots() {
+  [
+    ["plt1", "static/img/empty.png"],
+    ["plt2", "static/img/empty.png"],
+    ["plt3", "static/img/empty.png"],
+  ].forEach((plt) => {
+    document
+      .getElementById(plt[0])
+      .setAttribute("src", plt[1] + "?t=" + new Date().getTime());
+  });
+}
